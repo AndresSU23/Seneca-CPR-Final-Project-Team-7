12dcc42ecd0613baee93e81e7ac8cce7e92f27e9
DASUSeneca <111714730+AndresSU23@users.noreply.github.com>
2023-08-07 17:01:57 -0400

    Phrase Tokenizing

--- tokenizing.c
+++ tokenizing.c
@@ -11,10 +11,11 @@ Email: dasanchez-umbarila@myseneca.ca
 #include "tokenizing.h"
 // V1
 /**
- * This function takes numerous words from the user separated 
- * by spaces and stops taking input when it encounters a q 
- * character after that it prints the individual words and 
- * iterates again till it finds the q character on its own
+ * This function takes numerous words or phrases from the 
+ * user separated by spaces or commas and stops taking 
+ * input when it encounters a q character after that it 
+ * prints the individual words or phrase and iterates 
+ * again till it finds the q character on its own
  * @param None
  * @return Nones
 */
@@ -27,7 +28,7 @@ void tokenizing(void)
     int wordsCounter;
     do
     {
-
+// V1
         printf("Type a few words separated by space (q - to quit):\n"); 
         //Gets the input of the user and stores it in words
         fgets(words, BUFFER__SIZE, stdin);
@@ -48,6 +49,39 @@ void tokenizing(void)
             }
         }
     } while (strcmp(words, "q") != 0);
-    printf("*** End of Tokenizing Words Demo ***\n\n ");
+    printf("*** End of Tokenizing Words Demo ***\n\n");
+
+// V2
+    printf("*** Start of Tokenizing Phrases Demo ***\n\n");
+    // The initialization of variables
+    char phrases[BUFFER__SIZE];
+    char* nextPhrase = NULL;
+    int phrasesCounter;
+    do
+    {
+        printf("Type a few phrases separated by comma(q - to quit):\n");
+        //Gets the user standard input separated by ,
+        fgets(phrases, BUFFER__SIZE, stdin);
+        //Replaces the last character with a null terminator
+        phrases[strlen(phrases) - 1] = '\0';
+        //Continues while phrases isn't exclusively q
+        if ((strcmp(phrases, "q") != 0))
+        {
+            //Divides the string by comas
+            nextPhrase = strtok(phrases, ",");
+            //Initialize the phrase counter
+            phrasesCounter = 1;
+            //Iterate while there exists a next word
+            while (nextPhrase)
+            {
+                //Display the next phrase and its counter
+                printf("Phrase #%d is \'%s\'\n", phrasesCounter++, nextPhrase);
+                //Keeps tokenizing the fist passed string
+                nextPhrase = strtok(NULL, ",");
+            }
+        }
+    //Exit Condition for the loop
+    } while (strcmp(phrases, "q") != 0);
+    printf("*** End of Tokenizing Phrases Demo ***\n\n");
 }
     
